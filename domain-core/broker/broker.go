cloud-gov/cf-cdn-service-broker

Support for specifying headers to forward
In order to route custom domains to apps we offer a CDN using AWS
CloudFront. By default CloudFront only forwards a limited set of
HTTP headers, but our users have asked to forward more headers to
their apps (for instance the `Accept` header to indicate the
`Content-Type` to return.)

This commit adds a `headers` field to the provisioning details, which can
optionally be given as an array of strings. For instance:

 - {…, "headers": []} or omitting `headers` altogether will forward the
   `Host` header and Amazon's default headers.

 - {…, "headers": ["Accept"]} will forward the `Accept` header to the app,
   as well as `Host` and Amazon's default headers.

 - {…, "headers": ["*"]} will forward all headers and Amazon disables
   caching.

Amazon caps the number of whitelisted headers (as in the `Accept` example
above) to 10. We use `Host` of this 10 so that the CloudFoundry router
can map the custom domain to the app. This leaves 9 for our users, and if
they require more they can use the `*` wildcard header above.

CloudFront's caching mechanism maps forwarded HTTP requests to their
response. Only forwarding a limited set of headers by default maximises
the effectiveness of its cache. Therefore the more headers one forwards
(and the more variability within them) the less effective its caching
will be. We intend to warn about this in the documentation.

This commit also changes the type used for forwarded headers. Previously
they were passed around as a slice of strings, but our changes needed to
work with canonicalised headers and check for duplicates. It proved
cleaner and more natural to pass around a new `Headers` type, which
stores canonicalised headers and automatically deduplicates. This is
only converted to a `[]string` inside the CloudFront API utility.
`Headers` is defined in the `utils` package to avoid circular dependency.

broker/broker.go
@@ -14,14 +14,16 @@ import (
 	"github.com/18F/cf-cdn-service-broker/cf"
 	"github.com/18F/cf-cdn-service-broker/config"
 	"github.com/18F/cf-cdn-service-broker/models"
+	"github.com/18F/cf-cdn-service-broker/utils"
 )
 
 type Options struct {
-	Domain         string `json:"domain"`
-	Origin         string `json:"origin"`
-	Path           string `json:"path"`
-	InsecureOrigin bool   `json:"insecure_origin"`
-	Cookies        bool   `json:"cookies"`
+	Domain         string   `json:"domain"`
+	Origin         string   `json:"origin"`
+	Path           string   `json:"path"`
+	InsecureOrigin bool     `json:"insecure_origin"`
+	Cookies        bool     `json:"cookies"`
+	Headers        []string `json:"headers"`
 }
 
 type CdnServiceBroker struct {
@@ -80,7 +82,10 @@ func (b *CdnServiceBroker) Provision(
 		return spec, brokerapi.ErrInstanceAlreadyExists
 	}
 
-	headers := b.getHeaders(options)
+	headers, err := b.getHeaders(options)
+	if err != nil {
+		return spec, err
+	}
 
 	tags := map[string]string{
 		"Organization": details.OrganizationGUID,
@@ -208,7 +213,10 @@ func (b *CdnServiceBroker) Update(
 		return brokerapi.UpdateServiceSpec{}, err
 	}
 
-	headers := b.getHeaders(options)
+	headers, err := b.getHeaders(options)
+	if err != nil {
+		return brokerapi.UpdateServiceSpec{}, err
+	}
 
 	err = b.manager.Update(instanceID, options.Domain, options.Origin, options.Path, options.InsecureOrigin, headers, options.Cookies)
 	if err != nil {
@@ -227,6 +235,7 @@ func (b *CdnServiceBroker) createBrokerOptions(details []byte) (options Options,
 	options = Options{
 		Origin:  b.settings.DefaultOrigin,
 		Cookies: true,
+		Headers: []string{},
 	}
 	err = json.Unmarshal(details, &options)
 	if err != nil {
@@ -252,6 +261,10 @@ func (b *CdnServiceBroker) parseProvisionDetails(details brokerapi.ProvisionDeta
 			return
 		}
 	}
+	if len(options.Headers) > 9 {
+		err = errors.New("must pass no more than 9 custom headers to forward")
+		return
+	}
 	return
 }
 
@@ -271,6 +284,10 @@ func (b *CdnServiceBroker) parseUpdateDetails(details brokerapi.UpdateDetails) (
 		if err != nil {
 			return
 		}
+	}
+	if len(options.Headers) > 9 {
+		err = errors.New("must pass no more than 9 custom headers to forward")
+		return
 	}
 	return
 }
@@ -307,9 +324,26 @@ func (b *CdnServiceBroker) checkDomain(domain, orgGUID string) error {
 	return nil
 }
 
-func (b *CdnServiceBroker) getHeaders(options Options) []string {
-	if options.Origin == b.settings.DefaultOrigin {
-		return []string{"Host"}
+func (b *CdnServiceBroker) getHeaders(options Options) (headers utils.Headers, err error) {
+	headers = utils.Headers{}
+	for _, header := range options.Headers {
+		if headers.Contains(header) {
+			err = fmt.Errorf("must not pass duplicated header '%s'", header)
+			return
+		}
+		headers.Add(header)
 	}
-	return []string{}
+
+	// Forbid accompanying a wildcard with specific headers.
+	if headers.Contains("*") && len(headers) > 1 {
+		err = fmt.Errorf("must not pass whitelisted headers alongside wildcard")
+		return
+	}
+
+	// Ensure the Host header is forwarded if using a CloudFoundry origin.
+	if options.Origin == b.settings.DefaultOrigin && !headers.Contains("*") {
+		headers.Add("Host")
+	}
+
+	return
 }

utils/headers.go
@@ -1,0 +1,26 @@
+package utils
+
+import (
+	"net/textproto"
+)
+
+type Headers map[string]bool
+
+func (h Headers) Add(header string) {
+	canonical_header := textproto.CanonicalMIMEHeaderKey(header)
+	h[canonical_header] = true
+}
+
+func (h Headers) Contains(header string) bool {
+	canonical_header := textproto.CanonicalMIMEHeaderKey(header)
+	_, is_present := h[canonical_header]
+	return is_present
+}
+
+func (h Headers) Strings() []string {
+	headers := []string{}
+	for header, _ := range h {
+		headers = append(headers, header)
+	}
+	return headers
+}

utils/headers_test.go
@@ -1,0 +1,39 @@
+package utils_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	. "github.com/18F/cf-cdn-service-broker/utils"
+)
+
+func TestHeaders(t *testing.T) {
+	suite.Run(t, new(HeadersSuite))
+}
+
+type HeadersSuite struct {
+	suite.Suite
+}
+
+func (h *HeadersSuite) SetupTest() {}
+
+func (h *HeadersSuite) TestAdd() {
+	headers := Headers{}
+	headers.Add("abc-def")
+	h.Equal(headers, Headers{"Abc-Def": true})
+}
+
+func (h *HeadersSuite) TestContains() {
+	headers := Headers{"Abc-Def": true}
+	h.True(headers.Contains("Abc-Def"))
+	h.False(headers.Contains("Ghi-Jkl"))
+}
+
+func (h *HeadersSuite) TestStrings() {
+	headers := Headers{"Abc-Def": true, "User-Agent": true}
+	header_strings := headers.Strings()
+	h.Contains(header_strings, "Abc-Def")
+	h.Contains(header_strings, "User-Agent")
+	h.Equal(len(header_strings), 2)
+}

cloud-gov/cf-domain-broker

domain-core/broker/broker.go
@@ -14,14 +14,16 @@ import (
 	"github.com/18F/cf-cdn-service-broker/cf"
 	"github.com/18F/cf-cdn-service-broker/config"
 	"github.com/18F/cf-cdn-service-broker/models"
+	"github.com/18F/cf-cdn-service-broker/utils"
 )
 
 type Options struct {
-	Domain         string `json:"domain"`
-	Origin         string `json:"origin"`
-	Path           string `json:"path"`
-	InsecureOrigin bool   `json:"insecure_origin"`
-	Cookies        bool   `json:"cookies"`
+	Domain         string   `json:"domain"`
+	Origin         string   `json:"origin"`
+	Path           string   `json:"path"`
+	InsecureOrigin bool     `json:"insecure_origin"`
+	Cookies        bool     `json:"cookies"`
+	Headers        []string `json:"headers"`
 }
 
 type CdnServiceBroker struct {
@@ -80,7 +82,10 @@ func (b *CdnServiceBroker) Provision(
 		return spec, brokerapi.ErrInstanceAlreadyExists
 	}
 
-	headers := b.getHeaders(options)
+	headers, err := b.getHeaders(options)
+	if err != nil {
+		return spec, err
+	}
 
 	tags := map[string]string{
 		"Organization": details.OrganizationGUID,
@@ -208,7 +213,10 @@ func (b *CdnServiceBroker) Update(
 		return brokerapi.UpdateServiceSpec{}, err
 	}
 
-	headers := b.getHeaders(options)
+	headers, err := b.getHeaders(options)
+	if err != nil {
+		return brokerapi.UpdateServiceSpec{}, err
+	}
 
 	err = b.manager.Update(instanceID, options.Domain, options.Origin, options.Path, options.InsecureOrigin, headers, options.Cookies)
 	if err != nil {
@@ -227,6 +235,7 @@ func (b *CdnServiceBroker) createBrokerOptions(details []byte) (options Options,
 	options = Options{
 		Origin:  b.settings.DefaultOrigin,
 		Cookies: true,
+		Headers: []string{},
 	}
 	err = json.Unmarshal(details, &options)
 	if err != nil {
@@ -252,6 +261,10 @@ func (b *CdnServiceBroker) parseProvisionDetails(details brokerapi.ProvisionDeta
 			return
 		}
 	}
+	if len(options.Headers) > 9 {
+		err = errors.New("must pass no more than 9 custom headers to forward")
+		return
+	}
 	return
 }
 
@@ -271,6 +284,10 @@ func (b *CdnServiceBroker) parseUpdateDetails(details brokerapi.UpdateDetails) (
 		if err != nil {
 			return
 		}
+	}
+	if len(options.Headers) > 9 {
+		err = errors.New("must pass no more than 9 custom headers to forward")
+		return
 	}
 	return
 }
@@ -307,9 +324,26 @@ func (b *CdnServiceBroker) checkDomain(domain, orgGUID string) error {
 	return nil
 }
 
-func (b *CdnServiceBroker) getHeaders(options Options) []string {
-	if options.Origin == b.settings.DefaultOrigin {
-		return []string{"Host"}
+func (b *CdnServiceBroker) getHeaders(options Options) (headers utils.Headers, err error) {
+	headers = utils.Headers{}
+	for _, header := range options.Headers {
+		if headers.Contains(header) {
+			err = fmt.Errorf("must not pass duplicated header '%s'", header)
+			return
+		}
+		headers.Add(header)
 	}
-	return []string{}
+
+	// Forbid accompanying a wildcard with specific headers.
+	if headers.Contains("*") && len(headers) > 1 {
+		err = errors.New("must not pass whitelisted headers alongside wildcard")
+		return
+	}
+
+	// Ensure the Host header is forwarded if using a CloudFoundry origin.
+	if options.Origin == b.settings.DefaultOrigin && !headers.Contains("*") {
+		headers.Add("Host")
+	}
+
+	return
 }

domain-core/utils/headers.go
@@ -1,0 +1,26 @@
+package utils
+
+import (
+	"net/textproto"
+)
+
+type Headers map[string]bool
+
+func (h Headers) Add(header string) {
+	canonicalHeader := textproto.CanonicalMIMEHeaderKey(header)
+	h[canonicalHeader] = true
+}
+
+func (h Headers) Contains(header string) bool {
+	canonicalHeader := textproto.CanonicalMIMEHeaderKey(header)
+	_, isPresent := h[canonicalHeader]
+	return isPresent
+}
+
+func (h Headers) Strings() []string {
+	headers := []string{}
+	for header, _ := range h {
+		headers = append(headers, header)
+	}
+	return headers
+}

domain-core/utils/headers_test.go
@@ -1,0 +1,39 @@
+package utils_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	. "github.com/18F/cf-cdn-service-broker/utils"
+)
+
+func TestHeaders(t *testing.T) {
+	suite.Run(t, new(HeadersSuite))
+}
+
+type HeadersSuite struct {
+	suite.Suite
+}
+
+func (h *HeadersSuite) SetupTest() {}
+
+func (h *HeadersSuite) TestAdd() {
+	headers := Headers{}
+	headers.Add("abc-def")
+	h.Equal(headers, Headers{"Abc-Def": true})
+}
+
+func (h *HeadersSuite) TestContains() {
+	headers := Headers{"Abc-Def": true}
+	h.True(headers.Contains("Abc-Def"))
+	h.False(headers.Contains("Ghi-Jkl"))
+}
+
+func (h *HeadersSuite) TestStrings() {
+	headers := Headers{"Abc-Def": true, "User-Agent": true}
+	headerStrings := headers.Strings()
+	h.Contains(headerStrings, "Abc-Def")
+	h.Contains(headerStrings, "User-Agent")
+	h.Equal(len(headerStrings), 2)
+}