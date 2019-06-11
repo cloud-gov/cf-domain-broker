96dfe4d78f9cbd2da651c05d9207584b8e75e68f
Mike Lloyd <mike.lloyd@gsa.gov>
2019-06-11 17:06:36 -0600

    laid framework for let's encrypt integration tests
    Signed-off-by: Mike Lloyd <mike.lloyd@gsa.gov>

--- broker/broker_test.go
+++ broker/broker_test.go
@@ -3,17 +3,38 @@ package broker
 import (
 	"code.cloudfoundry.org/lager"
 	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
 	"database/sql"
+	"encoding/pem"
 	"github.com/18f/cf-domain-broker/fakes"
 	"github.com/18f/cf-domain-broker/models"
 	"github.com/18f/cf-domain-broker/routes"
 	"github.com/18f/cf-domain-broker/types"
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/jinzhu/gorm"
+	"github.com/jmhodges/clock"
+	"github.com/letsencrypt/pebble/ca"
+	"github.com/letsencrypt/pebble/db"
+	"github.com/letsencrypt/pebble/va"
+	"github.com/letsencrypt/pebble/wfe"
+	"github.com/miekg/dns"
 	"github.com/pivotal-cf/brokerapi/domain"
+	"github.com/spf13/afero"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+	"io/ioutil"
+	"log"
+	"math/big"
+	"net/http"
+	"os"
+	"strconv"
+	"syscall"
 	"testing"
+	"time"
 )
 
 // Broker test entry point.
@@ -32,6 +53,13 @@ type BrokerSuite struct {
 	DB   *gorm.DB
 	Mock sqlmock.Sqlmock
 }
+
+
+var (
+	testDnsRecords = map[string]string{}
+	kill chan syscall.Signal
+	pebbleServer http.Server
+)
 
 // This sets up the test suite before each test.
 func (s *BrokerSuite) SetupSuite() {
@@ -67,6 +95,15 @@ func (s *BrokerSuite) SetupSuite() {
 	}
 
 	s.Broker = NewDomainBroker(s.Manager, rms)
+
+	kill = make(chan syscall.Signal, 10)
+}
+
+// Teardown after each test.
+func (s *BrokerSuite) TearDownTestSuite() {
+	for i := 0; i < 10; i++ {
+		kill <- syscall.SIGKILL
+	}
 }
 
 func (s *BrokerSuite) TestDomainBroker_Services() {
@@ -100,4 +137,161 @@ func (s *BrokerSuite) TestDomainBroker_LastBindingOperation() {
 	b := s.Broker
 	_, err := b.LastBindingOperation(context.Background(), "", "", domain.PollDetails{})
 	s.NotNil(err, "expected error on last binding operation")
+}
+
+// startPebble will start the Pebble ACME server so we can have a local LE integration testing environment.
+func startPebble(s *suite.Suite) {
+
+	// create the base certificate fields.
+	certificate := &x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject: pkix.Name{
+			Organization: []string{"General Services Administration"},
+			Country:      []string{"US"},
+			Province:     []string{"CO"},
+			Locality:     []string{"Boulder"},
+			PostalCode:   []string{"80301"},
+		},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(10, 0, 0),
+		IsCA:                  true,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		BasicConstraintsValid: true,
+	}
+
+	// create a cert.
+	priv, _ := rsa.GenerateKey(rand.Reader, 2048)
+	pub := &priv.PublicKey
+	ca_b, err := x509.CreateCertificate(rand.Reader, certificate, certificate, pub, priv)
+	if err != nil {
+		s.Error(err, "expected no error creating certificate")
+	}
+
+	// in-memory filesystem.
+	fs := afero.NewMemMapFs()
+
+	// save the certificate cert.
+	caCrt, err := fs.Create("certificate.crt")
+	if err != nil {
+		s.Error(err, "expected no error creating certificate cert in-memory file.")
+	}
+	if err := pem.Encode(caCrt, &pem.Block{Type: "CERTIFICATE", Bytes: ca_b}); err != nil {
+		s.Error(err, "expected no error generating certificate pair")
+	}
+	defer caCrt.Close()
+
+	// save the certificate private key.
+	caKey, err := fs.Create("certificate.key")
+	if err != nil {
+		s.Error(err, "expected no error creating certificate key in-memory file")
+	}
+	if err := pem.Encode(caKey, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)}); err != nil {
+		s.Error(err, "expected no error generating certificate pair")
+	}
+	defer caKey.Close()
+
+	cacrt, err := ioutil.ReadAll(caCrt)
+	if err != nil {
+		s.Error(err, "expected no error reading public key")
+	}
+	cakey, err := ioutil.ReadAll(caKey)
+	if err != nil {
+		s.Error(err, "expected no error reading private key")
+	}
+
+	// create the pebble config.
+	config := &struct {
+		Pebble struct {
+			ListenAddress string
+			HTTPPort      int
+			TLSPort       int
+			Certificate   string
+			PrivateKey    string
+		}
+	}{
+		Pebble: struct {
+			ListenAddress string
+			HTTPPort      int
+			TLSPort       int
+			Certificate   string
+			PrivateKey    string
+		}{ListenAddress: "0.0.0.0:14000", HTTPPort: 5002, TLSPort: 5001, Certificate: string(cacrt), PrivateKey: string(cakey)},
+	}
+
+	// build our DNS server
+	go buildDnsTestServer(s)
+
+	// set up pebble.
+	logger := log.New(os.Stdout, "test", 0)
+	clk := clock.New()
+	localDb := db.NewMemoryStore(clk)
+	localCa := ca.New(logger, localDb)
+	localVa := va.New(logger, clk, 5002, 5001, false)
+
+	wfeImpl := wfe.New(logger, clk, localDb, localVa, localCa, false)
+	muxHandler := wfeImpl.Handler()
+
+	logger.Printf("Listening on: %s\n", config.Pebble.ListenAddress)
+	logger.Printf("ACME directory available at: https://%s%s",
+		config.Pebble.ListenAddress, wfe.DirectoryPath)
+	logger.Printf("Root CA certificate available at: https://%s%s",
+		config.Pebble.ListenAddress, wfe.RootCertPath)
+	pebbleServer = http.Server{
+		Addr: "0.0.0.0:14000",
+		TLSConfig: &tls.Config{
+
+		},
+	}
+	err = http.ListenAndServeTLS(
+		config.Pebble.ListenAddress,
+		config.Pebble.Certificate,
+		config.Pebble.PrivateKey,
+		muxHandler)
+}
+
+func buildDnsTestServer(s *suite.Suite) {
+	// attach request handler func
+	dns.HandleFunc("service.", handleDnsRequest)
+
+	t := s.T()
+
+	// start server
+	port := 5353
+	server := &dns.Server{Addr: ":" + strconv.Itoa(port), Net: "udp"}
+	t.Logf("Starting at %d\n", port)
+	err := server.ListenAndServe()
+	defer server.Shutdown()
+	if err != nil {
+		t.Fatalf("Failed to start server: %s\n ", err.Error())
+	}
+}
+
+var records = map[string]string{}
+
+func parseQuery(m *dns.Msg) {
+	for _, q := range m.Question {
+		switch q.Qtype {
+		case dns.TypeTXT:
+			txt := records[q.Name]
+			if txt != "" {
+				rr, _ := dns.NewRR(txt)
+				m.Answer = append(m.Answer, rr)
+			}
+		}
+
+	}
+}
+
+func handleDnsRequest(w dns.ResponseWriter, r *dns.Msg) {
+	m := new(dns.Msg)
+	m.SetReply(r)
+	m.Compress = false
+
+	switch r.Opcode {
+	case dns.OpcodeQuery:
+		parseQuery(m)
+	}
+
+	w.WriteMsg(m)
 }
--- go.mod
+++ go.mod
@@ -7,83 +7,39 @@ require (
 	code.cloudfoundry.org/lager v2.0.0+incompatible
 	github.com/18F/cf-cdn-service-broker v0.0.0-20190603161008-b2df4416bef5
 	github.com/18F/cf-domain-broker-alb v0.0.0-20190604210022-931b786921e7
-	github.com/Azure/azure-sdk-for-go v30.0.0+incompatible // indirect
-	github.com/Azure/go-autorest/autorest v0.2.0 // indirect
-	github.com/Azure/go-autorest/autorest/azure/auth v0.1.0 // indirect
-	github.com/Azure/go-autorest/autorest/to v0.2.0 // indirect
-	github.com/Azure/go-autorest/autorest/validation v0.1.0 // indirect
 	github.com/DATA-DOG/go-sqlmock v1.3.3
-	github.com/JamesClonk/vultr v2.0.0+incompatible // indirect
-	github.com/OpenDNS/vegadns2client v0.0.0-20180418235048-a3fa4a771d87 // indirect
-	github.com/akamai/AkamaiOPEN-edgegrid-golang v0.7.4 // indirect
-	github.com/aliyun/alibaba-cloud-sdk-go v0.0.0-20190606102855-d56e1148c593 // indirect
 	github.com/aws/aws-sdk-go v1.13.0
 	github.com/cenkalti/backoff v2.1.1+incompatible // indirect
-	github.com/cloudflare/cloudflare-go v0.9.1 // indirect
 	github.com/cloudfoundry-community/go-cfclient v0.0.0-20190611131856-16c98753d315
-	github.com/cpu/goacmedns v0.0.1 // indirect
-	github.com/decker502/dnspod-go v0.2.0 // indirect
-	github.com/dnsimple/dnsimple-go v0.23.0 // indirect
-	github.com/exoscale/egoscale v0.18.1 // indirect
-	github.com/fatih/structs v1.1.0 // indirect
+	github.com/drewolson/testflight v1.0.0 // indirect
 	github.com/go-acme/lego v2.6.0+incompatible
-	github.com/go-errors/errors v1.0.1 // indirect
 	github.com/go-ini/ini v1.42.0 // indirect
-	github.com/golang/mock v1.3.1 // indirect
-	github.com/google/btree v1.0.0 // indirect
-	github.com/google/go-querystring v1.0.0 // indirect
-	github.com/google/gofuzz v1.0.0 // indirect
-	github.com/google/pprof v0.0.0-20190515194954-54271f7e092f // indirect
 	github.com/google/uuid v1.1.1 // indirect
-	github.com/gophercloud/gophercloud v0.1.0 // indirect
 	github.com/gopherjs/gopherjs v0.0.0-20190430165422-3e4dfb77656c // indirect
 	github.com/gorilla/mux v1.7.2 // indirect
-	github.com/iij/doapi v0.0.0-20190504054126-0bbf12d6d7df // indirect
 	github.com/jinzhu/gorm v1.9.8
 	github.com/jmcarp/lego v0.3.2-0.20170424160445-b4deb96f1082
-	github.com/juju/ratelimit v1.0.1 // indirect
+	github.com/jmespath/go-jmespath v0.0.0-20180206201540-c2b33e8439af // indirect
+	github.com/jmhodges/clock v0.0.0-20160418191101-880ee4c33548
 	github.com/kelseyhightower/envconfig v1.4.0
-	github.com/kolo/xmlrpc v0.0.0-20190514182600-74b23a09d7ea // indirect
-	github.com/kr/pty v1.1.5 // indirect
-	github.com/labbsr0x/bindman-dns-webhook v1.0.0 // indirect
-	github.com/labbsr0x/goh v0.0.0-20190510171137-94bcf1cb07b7 // indirect
+	github.com/letsencrypt/pebble v1.0.1
 	github.com/lib/pq v1.1.1
-	github.com/linode/linodego v0.9.0 // indirect
-	github.com/maxbrunsfeld/counterfeiter/v6 v6.2.0 // indirect
-	github.com/miekg/dns v1.1.13 // indirect
-	github.com/mitchellh/mapstructure v1.1.2 // indirect
-	github.com/namedotcom/go v0.0.0-20180403034216-08470befbe04 // indirect
-	github.com/nrdcg/auroradns v1.0.0 // indirect
-	github.com/nrdcg/goinwx v0.6.0 // indirect
+	github.com/miekg/dns v1.1.14
 	github.com/onsi/ginkgo v1.8.0 // indirect
-	github.com/oracle/oci-go-sdk v5.9.0+incompatible // indirect
-	github.com/ovh/go-ovh v0.0.0-20181109152953-ba5adb4cf014 // indirect
+	github.com/onsi/gomega v1.5.0 // indirect
 	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
+	github.com/pborman/uuid v1.2.0 // indirect
 	github.com/pivotal-cf/brokerapi v5.0.0+incompatible
-	github.com/rainycape/memcache v0.0.0-20150622160815-1031fa0ce2f2 // indirect
 	github.com/robfig/cron v1.1.0
-	github.com/sacloud/libsacloud v1.22.5 // indirect
-	github.com/satori/go.uuid v1.2.0
-	github.com/sirupsen/logrus v1.4.2 // indirect
 	github.com/smartystreets/assertions v1.0.0 // indirect
+	github.com/spf13/afero v1.2.2
 	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/stretchr/testify v1.3.0
-	github.com/timewasted/linode v0.0.0-20160829202747-37e84520dcf7 // indirect
-	github.com/transip/gotransip v5.8.2+incompatible // indirect
-	github.com/urfave/cli v1.20.0 // indirect
-	github.com/xenolf/lego v2.6.0+incompatible // indirect
-	go.opencensus.io v0.22.0 // indirect
-	golang.org/x/exp v0.0.0-20190510132918-efd6b22b2522 // indirect
-	golang.org/x/image v0.0.0-20190523035834-f03afa92d3ff // indirect
-	golang.org/x/mobile v0.0.0-20190607214518-6fa95d984e88 // indirect
-	golang.org/x/mod v0.1.0 // indirect
+	github.com/xenolf/lego v2.6.0+incompatible
 	golang.org/x/net v0.0.0-20190611141213-3f473d35a33a // indirect
 	golang.org/x/sys v0.0.0-20190610200419-93c9922d18ae // indirect
-	golang.org/x/tools v0.0.0-20190611154301-25a4f137592f // indirect
 	google.golang.org/appengine v1.6.1 // indirect
-	google.golang.org/genproto v0.0.0-20190605220351-eb0b1bdb6ae6 // indirect
-	google.golang.org/grpc v1.21.1 // indirect
-	gopkg.in/ns1/ns1-go.v2 v2.0.0-20190430170845-6c599e5e5790 // indirect
+	gopkg.in/ini.v1 v1.42.0 // indirect
+	gopkg.in/square/go-jose.v1 v1.1.2 // indirect
 	gopkg.in/square/go-jose.v2 v2.3.1 // indirect
-	honnef.co/go/tools v0.0.0-20190607181801-497c8f037f5a // indirect
 )
