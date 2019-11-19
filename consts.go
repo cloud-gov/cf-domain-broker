0410fb8ec59e4b602409016ea0ee1d5098ee96f8
Mike Lloyd <mike.lloyd@gsa.gov>
2019-11-19 14:40:08 -0700

    added some of the core metadata.
    Signed-off-by: Mike Lloyd <mike.lloyd@gsa.gov>

--- consts.go
+++ consts.go
@@ -3,22 +3,22 @@ package cf_domain_broker
 import "time"
 
 const (
-	DomainServiceName = "custom-domain-broker"
+	DomainServiceName = "custom-domain"
 	DomainServiceId   = "7428b169-f693-46c8-86bc-6dc8a8ea1361"
 
 	// todo (mxplusb): update this.
-	DomainServiceDescription                 = "Service Broker description to go here."
-	DomainServiceMetadataDisplayName         = "display"
+	DomainServiceDescription                 = "Create a custom TLS certificate for your application."
+	DomainServiceMetadataDisplayName         = "Custom TLS Certificate"
 	DomainServiceMetadataLongDescription     = "long"
-	DomainServiceMetadataDocumentationUrl    = "https://google.com"
+	DomainServiceMetadataDocumentationUrl    = "https://github.com/18f/cf-domain-broker/tree/master/README.md"
 	DomainServiceMetadataSupportUrl          = "https://cloud.gov/support"
 	DomainServiceMetadataImageUrl            = "some-image"
-	DomainServiceMetadataProviderDisplayName = "something"
+	DomainServiceMetadataProviderDisplayName = "cloud.gov"
 
 	CDNPlanName = "domain-with-cdn-and-ssl"
 	CDNPlanId   = "a0bc63d1-cd2a-44e5-a709-396b382676f0"
 
-	DomainPlanName = "domain-with-ssl"
+	DomainPlanName = "domain-with-tls"
 	DomainPlanId   = "5d4bcdef-efa1-4b0a-b658-7776c3438e4a"
 
 	MaxHeaderCount = 10
