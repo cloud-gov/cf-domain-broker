69d411a0a10b65706668511774c80f3f98e5f627
Tammer Saleh <tammer@superorbit.al>
2019-12-18 00:33:57 +0000

    updated todo comment

--- doc.go
+++ doc.go
@@ -4,4 +4,5 @@ Docs for this service broker.
 
 // todo (mxplusb): add ci
 // todo (mxplusb): move back to cloudfoundry-community after cf-com/go-client#233 is resolved.
+//      (tsaleh): https://github.com/cloudfoundry-community/go-cfclient/pull/233 _is_ resolved?
 package cf_domain_broker
